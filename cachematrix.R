728f931555dcb0c4e8acb8e59137d40d0f72322b
Kateryna Lunina <lunina.kateryna@lll.kpi.ua>
2024-07-23 16:31:40 +0300

    CacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
+##(checks the matrix for degeneracy and squareness)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if (nrow(x)!=ncol(x)){
+    return("Matrix is a non-square and does not have an inverse")
+  }
+  else if (det(x)==0) {
+    return("Matrix is a degenerate and does not have an inverse")
+  }
+  else {
+    c <- NULL
+    set <- function(y) {
+      x <<- y
+      c <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) c <<- solve
+    getsolve <- function() c
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+  }
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  c <- x$getsolve()
+  if(!is.null(c)) {
+    message("getting inversed matrix")
+    return(c)
+  }
+  data <- x$get()
+  c <- solve(data, ...)
+  x$setsolve(c)
+  c
 }
+
+
